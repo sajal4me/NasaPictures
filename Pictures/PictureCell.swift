f817a0d1459507fc993f8149247c98babaece097
Sajal Gupta <sajal.gupta@tokopedia.com>
2021-11-22 22:22:15 +0530

    created xib of PictureCell that display the content

---
+++ Pictures/PictureCell.swift
@@ -0,0 +1,40 @@
+//
+//  PictureCell.swift
+//  Pictures
+//
+//  Created by Sajal Gupta on 21/11/21.
+//
+
+import UIKit
+import Kingfisher
+
+internal final class PictureCell: UICollectionViewCell {
+
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var dateLabel: UILabel!
+    @IBOutlet private weak var explanationLabel: UILabel!
+    @IBOutlet private weak var favouriteButton: UIButton! {
+        didSet {
+            favouriteButton.setTitle("", for: .normal)
+            favouriteButton.setImage(UIImage(named: "favourite"), for: .normal)
+            favouriteButton.setImage(UIImage(named: "filled_favourite"), for: .selected)
+        }
+    }
+    
+    internal var model: ImageModel? {
+        didSet {
+            imageView.kf.indicatorType = .activity
+            imageView.kf.setImage(with: model?.url, placeholder: UIImage(named: "placeholder"))
+            titleLabel.text = model?.title
+            dateLabel.text = model?.date
+            explanationLabel.text = model?.explanation
+        }
+    }
+
+    override internal func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.layer.cornerRadius = 8
+    }
+}
+
