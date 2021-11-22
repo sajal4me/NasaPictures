1f0cf4da818613fe702decf4f7da9f02866c5b26
Sajal Gupta <sajal.gupta@tokopedia.com>
2021-11-22 22:20:35 +0530

    create ImageViewModel

---
+++ Pictures/ViewModel/ImageViewModel.swift
@@ -0,0 +1,85 @@
+//
+//  ImageViewModel.swift
+//  Pictures
+//
+//  Created by Sajal Gupta on 21/11/21.
+//
+
+import Foundation
+
+internal final class ImageViewModel {
+    
+    private let usecase: NasaUsecaseProtocol
+    private var imageModel: [ImageModel] = []
+    private var startDate: Date = Date()
+    private var endDate: Date = Date()
+    private let dateFormatter = DateFormatter()
+    private let dateFormat = "yyyy-MM-dd"
+    
+    // closure to bind data
+    internal var didUpdateModel: (() -> Void)?
+    internal var didGetError: ((NetworkError) -> Void)?
+    
+    internal init() {
+        let nassaUsecase = NasaUsecase()
+        self.usecase = nassaUsecase
+        nassaUsecase.delegate = self
+        
+        dateFormatter.dateFormat = dateFormat
+        
+        // load Picture from API
+        usecase.fetchPictureOfDay(startDate: getFormattedStartDate, endDate: getFormattedEndtDate)
+    }
+}
+
+
+extension ImageViewModel: PictureDelegate {
+    internal func didLoad(result: Result<[ImageModel], NetworkError>) {
+        switch result {
+            
+        case let .success(model):
+            imageModel = model
+            didUpdateModel?()
+            
+        case let .failure(error):
+            didGetError?(error)
+        }
+    }
+    
+    internal var model: [ImageModel] {
+        imageModel
+    }
+    
+    internal var getStartDate: Date {
+        startDate
+    }
+    
+    internal var getFormattedStartDate: String {
+        dateFormatter.string(from: startDate)
+    }
+    
+    internal var getEndDate: Date {
+        endDate
+    }
+    
+    internal var getFormattedEndtDate: String {
+        dateFormatter.string(from: endDate)
+    }
+    
+    internal var getDateFormat: String {
+        dateFormat
+    }
+    internal func updateStartDate(_ newDate: Date) {
+        // current selected date is same as newDate then no need to load data from API
+        if startDate == newDate { return }
+        startDate = newDate
+        usecase.fetchPictureOfDay(startDate: getFormattedStartDate, endDate: getFormattedEndtDate)
+    }
+    
+    internal func updateEndDate(_ newDate: Date) {
+        // current selected date is same as newDate then no need to load data from API
+        if endDate == newDate { return }
+        endDate = newDate
+        usecase.fetchPictureOfDay(startDate: getFormattedStartDate, endDate: getFormattedEndtDate)
+    }
+}
